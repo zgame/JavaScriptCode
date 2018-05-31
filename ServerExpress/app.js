bcc5dc6c958d018c62e501602f874e9a6524c98e
zgame <zgamecn@gmail.com>
2018-05-31 17:57:39 +0800

    add
    add

--- ServerExpress/app.js
+++ ServerExpress/app.js
@@ -18,6 +18,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//--------------------------------跨域设置------------------------------------------
 let allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); //必须重新设置，把origin的域加上去
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -26,7 +27,7 @@ let allowCrossDomain = function (req, res, next) {
     next();
 };
 app.use(allowCrossDomain);
-
+//--------------------------------------------------------------------------
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -57,5 +58,10 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
+
+
+
 
 module.exports = app;
--- ServerExpress/routes/zsw.js
+++ ServerExpress/routes/zsw.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+let zswAction = require("../action/zswAction");
 
 /* GET users listing. */
-router.get('/:name', function(req, res, next) {
-
-    console.log("-------------:",req.params.name);
-    res.render('page-login', { title: req.params.name, list:[1,2,3,4,5] });
+router.get('/:name', function (req, res, next) {
+    console.log("-------------:", req.params.name);
+    res.render('page-login', {title: req.params.name, list: [1, 2, 3, 4, 5]});
 });
 
-router.get('/zswAction', function(req, res, next) {
-
-    console.log("-------------:",req.query);
-    res.render('page-login', { title: req.params.name, list:[1,2,3,4,5] });
+router.get('/', function (req, res, next) {
+    zswAction(req, res, next);
 });
-
-
 
 
 module.exports = router;
