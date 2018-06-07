bc435be9a9bf17c5d41321ef6efcb983f033a8c3
zgame <zgamecn@gmail.com>
2018-06-07 11:08:49 +0800

    add
    add

--- ServerExpress/action/recharge.js
+++ ServerExpress/action/recharge.js
@@ -5,18 +5,34 @@ let recharge = {};
 
 //-------------------------获取充值信息----------------------------
 recharge.getList = function (req, res, next) {
+    console.info('-------------recharge.getList--------------');
     let limit = req.query.limit;
     let page = req.query.page;
+    let uid = req.query.uid;
+    let channel = req.query.channel;
+    let serverid = req.query.serverid;
+
     let data = {};
     let connection = db.connection();
     let sql = "SELECT * FROM player_charge_rmb where time between '2018-01-01 00:00:00' and '2019-01-01 00:00:00'";
+    if (uid !== '') {
+        sql += "and user_id ='" + uid + "'";
+    }
+    if (channel !== '') {
+        sql += "and platform_id ='" + channel + "'";
+    }
+    if (serverid !== '') {
+        sql += "and server_id ='" + serverid + "'";
+    }
+    console.info('sql:'+sql);
+
     connection.query(sql, function (err, result) {
         if (err) {
             console.log('[SELECT  ERROR] - ', err.message);
             res.send(res_json(false, "", "数据库错误！"));
             return;
         }
-        data["items"] = result.slice(limit*(page-1),limit*page) ;
+        data["items"] = result.slice(limit * (page - 1), limit * page);
         data['total'] = result.length;
         res.send(res_json(true, data, "获取所有用户信息"));
     });
--- vue-element-admin-template/src/views/table/zsw.vue
+++ vue-element-admin-template/src/views/table/zsw.vue
@@ -2,22 +2,17 @@
   <div class="app-container">
     <!--**************************过滤*******************************-->
     <div class="filter-container">
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" :placeholder="$t('table.title')" v-model="listQuery.title"></el-input>
-      <!--<el-select clearable style="width: 90px" class="filter-item" v-model="listQuery.importance" :placeholder="$t('table.importance')">-->
-        <!--<el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item">-->
-        <!--</el-option>-->
-      <!--</el-select>-->
-      <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.type" :placeholder="$t('table.type')">
+      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="请输入UID" v-model="listQuery.uid"></el-input>
+
+      <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.channel" placeholder="渠道">
         <el-option v-for="item in  calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key">
         </el-option>
       </el-select>
-      <el-select @change='handleFilter' style="width: 140px" class="filter-item" v-model="listQuery.sort">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key">
-        </el-option>
-      </el-select>
+
+      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="ServerID" v-model="listQuery.serverid"></el-input>
+
 
       <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">搜索</el-button>
-      <!--<el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>-->
       <el-button class="filter-item" type="warning" :loading="downloadLoading" v-waves icon="el-icon-download" @click="handleDownload">导出Excel</el-button>
     </div>
     <br>
@@ -27,7 +22,10 @@
         <template slot-scope="scope"> {{scope.row.user_id}}</template>
       </el-table-column>
       <el-table-column label="渠道" width="100" align="center" sortable prop="platform_id">
-        <template slot-scope="scope"> {{scope.row.platform_id}}</template>
+        <template slot-scope="scope">
+          <span class="link-type">{{scope.row.title}}</span>
+          <el-tag>{{scope.row.platform_id | typeFilter}}</el-tag>
+        </template>
       </el-table-column>
       <el-table-column label="服务器" width="100" align="center" sortable prop="server_id">
         <template slot-scope="scope"><span>{{scope.row.server_id}}</span></template>
@@ -73,6 +71,19 @@
 <script>
   import { actionGetRechargeList } from '@/api/recharge'
 
+  const calendarTypeOptions = [
+    { key: '20', display_name: 'UC' },
+    { key: '888888', display_name: '自有渠道' },
+    { key: '255', display_name: '华为' },
+    { key: '368', display_name: 'vivo' }
+  ]
+
+  // arr to obj ,such as { CN : "China", US : "USA" }
+  const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
+    acc[cur.key] = cur.display_name
+    return acc
+  }, {})
+
   export default {
     data() {
       return {
@@ -91,10 +102,19 @@
         },
         listQuery: {
           page: 1,
-          limit: 10
+          limit: 10,
+          uid: '',
+          channel: '',
+          serverid: ''
         },
         total: 0,
         downloadLoading: false
+      }
+    },
+    filters: {
+
+      typeFilter(type) {
+        return calendarTypeKeyValue[type]
       }
     },
 
@@ -118,6 +138,11 @@
       },
       handleCurrentChange(val) {
         this.listQuery.page = val
+        this.getUserList()
+      },
+      // --------------------------------搜索--------------------------------
+      handleFilter() {
+        this.listQuery.page = 1
         this.getUserList()
       },
       // --------------------------------导出excel--------------------------------
