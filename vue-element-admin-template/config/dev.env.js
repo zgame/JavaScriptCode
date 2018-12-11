59bc88c893b14b27c18c99b50c2c0232e8a00f39
zgame <zgamecn@gmail.com>
2018-12-11 14:15:31 +0800

    add
    add

--- vue-element-admin-template/config/dev.env.js
+++ vue-element-admin-template/config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://localhost:5000"',
+  BASE_API: '"http://172.16.140.63:5000"',
 })
--- vue-element-admin-template/config/index.js
+++ vue-element-admin-template/config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '172.16.140.63', // can be overwritten by process.env.HOST
+    // host: 'localhost', // can be overwritten by process.env.HOST
     port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
